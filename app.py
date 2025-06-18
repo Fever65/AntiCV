1da840a7336f66bf135799f6e38271f9380f76aa
Fever <122129656+Fever65@users.noreply.github.com>
2025-06-18 18:44:33 +0200

    r
    r

--- app.py
+++ app.py
@@ -1,30 +1,19 @@
-from flask import Flask, request, render_template, redirect
-from flask_cors import CORS
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
-CORS(app)
+app = Flask(__name__)
 
-DB = 'anticv.db'
-
-def db_connection():
-    conn = sqlite3.connect(DB)
+def get_db_connection():
+    conn = sqlite3.connect('anticv.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-@app.route('/')
+@app.route("/", methods=["GET"])
 def index():
-    conn = db_connection()
-    cur = conn.cursor()
-    cur.execute('''
-        SELECT anticv.*, users.pseudo, users.pfp_ridicule, users.theme_couleur
-        FROM anticv
-        JOIN users ON anticv.user_id = users.id
-        WHERE visible = 1
-        ORDER BY created_at DESC
-    ''')
-    cvs = cur.fetchall()
-    return render_template('index.html', cvs=cvs)
+    conn = get_db_connection()
+    cvs = conn.execute("SELECT * FROM anticv").fetchall()
+    conn.close()
+    return render_template("index.html", cvs=cvs)
 
 @app.route("/submit_cv", methods=["POST"])
 def submit_cv():
@@ -41,16 +30,18 @@ def submit_cv():
     type_cv = request.form["type_cv"]
     pfp_ridicule = request.form["pfp_ridicule"]
 
-    conn = db_connection()
-    cur = conn.cursor()
-    cur.execute("INSERT INTO users (pseudo, email, password, pfp_ridicule, theme_couleur) VALUES (?, '', '', ?, ?)",
-                (pseudo, pfp, couleur))
-    user_id = cur.lastrowid
-    cur.execute("INSERT INTO anticv (user_id, titre, competences, experiences, ambitions) VALUES (?, ?, ?, ?, ?)",
-                (user_id, titre, competences, experiences, ambitions))
+    conn = get_db_connection()
+    conn.execute("""
+        INSERT INTO anticv (pseudo, titre, competences, experiences, ambitions,
+                            objectif_vie, hobbies, animal_totem, citation,
+                            theme, type_cv, pfp_ridicule)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """, (pseudo, titre, competences, experiences, ambitions,
+          objectif_vie, hobbies, animal_totem, citation, theme, type_cv, pfp_ridicule))
     conn.commit()
-    return redirect('/')
+    conn.close()
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000)
+    return redirect("/")
 
+if __name__ == "__main__":
+    app.run(debug=True)
